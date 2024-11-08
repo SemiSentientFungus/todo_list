4f818fc8c9803cf2abda5e84f681135d29c4616e
Cassandra <semisentientfungus@gmail.com>
2024-11-08 15:03:30 +0200

    begin DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css"
 
-import project from "./project,js"
-import todo from "./todo,js"
+import project from "./project.js"
+import todo from "./todo.js"
 
 const body = document.body;
+
