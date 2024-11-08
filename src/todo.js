000a3fbe36793ebebf47523e82417a0ce60d586b
Cassandra <semisentientfungus@gmail.com>
2024-11-08 14:25:52 +0200

    refractor todo object constructor

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,9 @@
-function todo(title, description, dueDate, priority) {
+export default function todo(title, description, dueDate, priority, notes, checklist) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+    this.completed = false;
 }
-
-export {todo};
